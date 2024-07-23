62bac1fcbad4baf9fb6bad3bd44b138c2a9458ee
SajalSatsangi <sajalsatsangi2004@gmail.com>
2024-07-23 13:32:16 +0530

    update

--- api/predict.py
+++ api/predict.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import joblib
 import numpy as np
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 # Load the trained model
 model = joblib.load('random_forest_model.pkl')
